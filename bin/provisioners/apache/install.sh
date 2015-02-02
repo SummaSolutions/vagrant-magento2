2d0c5055f3e429a05ba12bc8cbdfcb6b5b9be161
Facundo Capua <fcapua@summasolutions.net>
2015-02-01 21:30:39 -0300

    Change Apache use to vagrant

--- bin/provisioners/apache/install.sh
+++ bin/provisioners/apache/install.sh
@@ -3,6 +3,12 @@
 #Install Apache
 apt-get -y install apache2
 
+
+sed -i "s|\APACHE_RUN_USER=www-data|APACHE_RUN_USER=vagrant|g" /etc/apache2/envvars
+sed -i "s|\APACHE_RUN_GROUP=www-data|APACHE_RUN_GROUP=vagrant|g" /etc/apache2/envvars
+
+chown -R vagrant:vagrant /var/lock/apache2
+
 a2enmod rewrite
 a2enmod headers
 a2enmod php5
